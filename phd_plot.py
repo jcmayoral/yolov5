3b6c2f37834f1cc29c8706f0140ffd61e5b553bb
Jose <jocamaba1989@gmail.com>
2022-03-29 11:17:59 -0600

    plot v2

--- phd_plot.py
+++ phd_plot.py
@@ -7,12 +7,31 @@ import numpy as np
 
 plt.Figure()
 
+class DataContainer:
+    def __init__(self):
+        self.initialize()
+
+    def initialize(self):
+        self.data = dict()
+        for id in ["all", "Lethal", "Danger", "Warning", "Safe"]:
+            self.data[id] = dict()
+            for st in ['P', 'R','mAP@.5', 'mAP@.5:.95']:
+                self.data[id][st] = np.zeros(11)
+
+    def set(self, id, st, ind, value):
+        self.data[id][st][ind] = value
+
+    def get(self,id, st):
+        return self.data[id][st]
+
 def process(opt):
     samples = [0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]
     #linspace for confidence threshold
     for ind,s in enumerate(samples):
         precision = np.zeros(len(samples))
         recall = np.zeros(len(samples))
+
+        container = DataContainer()
 
         #look for files and read if matches with pattern
         for files in glob.glob(os.path.join(opt.folder,"results_"+str(s)+"*.txt")):
@@ -38,16 +57,26 @@ def process(opt):
 
             #iterate all, Lethal, Danger, Warning, Safe
             frame
-            for cl in ['all']: #frame['Class'].unique():
+            for cl in frame['Class'].unique():
                 data = frame[frame['Class'] == cl]
-                precision[n] = data['P']
-                recall[n] = data['R']
+                container.set(cl, 'P',n, data['P'])
+                container.set(cl, 'R',n, data['R'])
+                container.set(cl, 'mAP@.5',n, data['mAP@.5'])
+                container.set(cl, 'mAP@.5:.95',n, data['mAP@.5:.95'])
                 #print(data['P'], data['R'], data['mAP@.5'], data['mAP@.5:.95'])
         #sampling is the same in conf and iou
         print(precision, samples)
-        plt.plot(samples, precision)
-        plt.plot(samples, recall)
+        #plt.plot(samples, precision)
+        #plt.plot(samples, recall)
+        for cl in ['all', 'Lethal', 'Danger', 'Warning', "Safe"]:
+            for st in ['P', 'R', 'mAP@.5', 'mAP@.5:.95']:
+                plt.plot(samples, container.get(cl, st), label="Class {} Statics {}".format(cl,st))
+        #plt.plot(samples, container.get('all', 'R'), label="All Recall")
+        #plt.plot(samples, container.get('all', 'mAP@.5'), label="All mAP@.5")
+        #plt.plot(samples, container.get('all', 'mAP@.5:.95'), label="All mAP@.5:95")
+
         plt.title("conf {} vs iou".format(s))
+        plt.legend()
         plt.show()
 
 def parse_opt():

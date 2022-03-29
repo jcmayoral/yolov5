e860c37ca220354eaa5d007725078bc1d6a327c4
Jose <jocamaba1989@gmail.com>
2022-03-29 11:34:08 -0600

    seems to work

--- phd_plot.py
+++ phd_plot.py
@@ -24,12 +24,15 @@ class DataContainer:
 
 def process(opt):
     samples = [0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]
+    stats = ['P', 'R', 'mAP@.5', 'mAP@.5:.95']
+    classes = ['all', 'Lethal', 'Danger', 'Warning', "Safe"]
+
     #linspace for confidence threshold
     for ind,s in enumerate(samples):
         container = DataContainer()
         #one for stats
         fig, axs = plt.subplots(2,2)
-        fig.suptitle("conf {} vs iou".format(s))
+        fig.suptitle("Results Comparison NMS with Confidence Threshold set to  {}".format(s))
 
         #look for files and read if matches with pattern
         for files in glob.glob(os.path.join(opt.folder,"results_"+str(s)+"*.txt")):
@@ -51,33 +54,34 @@ def process(opt):
 
             #index from iou_thr
             n = int(float(iou_thr)*10)
-            print("INDEX", n)
 
             #iterate all, Lethal, Danger, Warning, Safe
             frame
             for cl in frame['Class'].unique():
                 data = frame[frame['Class'] == cl]
-                container.set(cl, 'P',n, data['P'])
-                container.set(cl, 'R',n, data['R'])
-                container.set(cl, 'mAP@.5',n, data['mAP@.5'])
-                container.set(cl, 'mAP@.5:.95',n, data['mAP@.5:.95'])
+                for st in stats:
+                    container.set(cl, st,n, data[st])
+                #container.set(cl, 'R',n, data['R'])
+                #container.set(cl, 'mAP@.5',n, data['mAP@.5'])
+                #container.set(cl, 'mAP@.5:.95',n, data['mAP@.5:.95'])
                 #print(data['P'], data['R'], data['mAP@.5'], data['mAP@.5:.95'])
         #sampling is the same in conf and iou
         #plt.plot(samples, precision)
         #plt.plot(samples, recall)
-        for cl in ['all', 'Lethal', 'Danger', 'Warning', "Safe"]:
-            for i,st in enumerate(['P', 'R', 'mAP@.5', 'mAP@.5:.95']):
+        for cl in classes:
+            for i,st in enumerate(stats):
                 r = i//2
-                print (r)
                 c = i%2
-                print(c)
                 axs[c,r].plot(samples, container.get(cl, st), label="Class {} Statics {}".format(cl,st))
 
         for i in range(4):
             r = i//2
             c = i%2
+            axs[c,r].set_title(stats[i])
             axs[c,r].legend()
             axs[c,r].grid()
+            axs[c,r].set_xlabel('IoU threshold')
+
         #plt.plot(samples, container.get('all', 'R'), label="All Recall")
         #plt.plot(samples, container.get('all', 'mAP@.5'), label="All mAP@.5")
         #plt.plot(samples, container.get('all', 'mAP@.5:.95'), label="All mAP@.5:95")

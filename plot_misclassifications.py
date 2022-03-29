cbfcf68c3b6ceb1dc7786ecd4f10da61a6a73429
Jose <jocamaba1989@gmail.com>
2022-03-29 16:25:22 -0600

    save

--- plot_misclassifications.py
+++ plot_misclassifications.py
@@ -14,14 +14,20 @@ def process(opt, normalize=1):
     for ind,s in enumerate(samples):
         #container = DataContainer()
         #one for stats
-        fig, axs = plt.subplots(5,5)
+        #fig, axs = plt.subplots(5,5)
+        fig, axs = plt.subplots(4)
+
         fig.suptitle("Confusion Matrix with Confidence Threshold set to  {}".format(s))
 
+        non_data = np.zeros((11, 5, 5))
         data = np.zeros((11, 5, 5))
+
+        number_files = 1
+        n_images = 1
 
         #look for files and read if matches with pattern
         for files in glob.glob(os.path.join(opt.folder,"cm_"+str(s)+"*.txt")):
-            cm = np.loadtxt(files, dtype=np.float, delimiter=' ')
+            non_cm = np.loadtxt(files, dtype=np.float, delimiter=' ')
             #Get confidence form file results_<conf>_<iou>
             #conf_thr == s
             fileName, _ = os.path.splitext(files)
@@ -32,22 +38,82 @@ def process(opt, normalize=1):
             #index from iou_thr
             n = int(float(iou_thr)*10)
 
-            cm = cm / ((cm.sum(0).reshape(1, -1) + 1E-9) if normalize else 1)  # normalize columns
+            cm = non_cm / ((non_cm.sum(0).reshape(1, -1) + 1E-9) if normalize else 1)  # normalize columns
 
+            #Assuming same size of set
+            number_files = non_cm.sum(0)
+            n_images = number_files[:4].sum()
 
             data[n] = cm
+            non_data[n] = non_cm
 
-        for i in range(5):
-            for j in range(5):
-                axs[i,j].plot(samples,  data[:,i,j], label="CM Index {} {}".format(i,j))
+        #
+        #for i in range(5):
+        #    for j in range(5):
+        #        axs[i,j].plot(samples,  data[:,i,j], label="CM Index {} {}".format(i,j))
 
-        for i in range(25):
-            r = i//5
-            c = i%5
-            axs[c,r].set_title("CM Index {} {}".format(i,j))
-            #axs[c,r].legend()
-            axs[c,r].grid()
-            axs[c,r].set_xlabel('IoU threshold')
+        #Precision
+        for j in range(4):
+            axs[0].plot(samples,  data[:,j,j], label="Precision Class {}".format(j))
+            axs[0].set_title("Normalize Precision")
+
+        #FP
+        for j in range(4):
+            axs[1].plot(samples,  data[:,j,-1], label="FP {}".format(j))
+            axs[1].set_title("False Positives Percentual")
+
+        #FN
+        for j in range(4):
+            axs[2].plot(samples,  non_data[:,-1,j], label="FN {}".format(j))
+            axs[2].set_title("Number of images of False Negatives")
+
+        #LETHAL MISCLASSIFICATIONS
+        lmisclass = non_data[:,0,1] + non_data[:,0,2] + non_data[:,0,3] + non_data[:,1,0] + non_data[:,2,0] + non_data[:,3,0]
+        axs[3].plot(samples, lmisclass/number_files[0] , label="Lethal")
+        #0,1 0,2 0,3
+        #1,0 2,0 3,0
+        axs[3].set_title("Misclassifications")
+
+        #Danger MISCLASSIFICATIONS
+        dmisclass = non_data[:,0,1] + non_data[:,2,1] + non_data[:,3,1] + non_data[:,1,0] + non_data[:,1,2] + non_data[:,1,3]
+        axs[3].plot(samples, dmisclass/number_files[1] , label="Danger")
+        #0,1 2,1 3,1
+        #1,0 1,2 1,3
+
+        #Warning MISCLASSIFICATIONS
+        wmisclass = non_data[:,2,0] + non_data[:,2,1] + non_data[:,2,3] + non_data[:,0,2] + non_data[:,1,2] + non_data[:,3,2]
+        axs[3].plot(samples, wmisclass/number_files[2] , label="Warning")
+        #2,0 2,1 2,3
+        #0,2 1,2 3,2
+
+        #Safe MISCLASSIFICATIONS
+        smisclass = non_data[:,3,0] + non_data[:,3,1] + non_data[:,3,2] + non_data[:,0,3] + non_data[:,1,3] + non_data[:,2,3]
+        axs[3].plot(samples, smisclass/number_files[3] , label="Safe")
+        #3,0 3,1 3,2
+        #0,3 1,3 2,3
+
+        #all
+        all_misclass = non_data[:,0,1] + non_data[:,0,2] + non_data[:,0,3] \
+                     + non_data[:,1,0] + non_data[:,1,2] + non_data[:,1,3] \
+                     + non_data[:,2,0] + non_data[:,2,1] + non_data[:,2,3] \
+                     + non_data[:,3,0] + non_data[:,3,1] + non_data[:,3,2]
+
+        axs[3].plot(samples, all_misclass/n_images , label="ALL")
+
+
+        for i in range(4):
+            axs[i].legend()
+            axs[i].grid()
+        axs[3].set_xlabel('IoU threshold')
+
+
+        #for i in range(25):
+        #    r = i//5
+        #    c = i%5
+        #    axs[c,r].set_title("CM Index {} {}".format(i,j))
+        #    #axs[c,r].legend()
+        #    axs[c,r].grid()
+        #    axs[c,r].set_xlabel('IoU threshold')
 
         plt.legend()
         plt.show()
